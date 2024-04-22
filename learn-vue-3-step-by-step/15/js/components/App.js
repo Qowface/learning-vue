d93ec7336e17a227402da1860c3abcea639b26a8
Robert Marquess <qowsrule@gmail.com>
2024-04-22 18:29:29 -0400

    Learn Vue 3: Step By Step - Named Slots - 2
    Add panel component with a named slot and a default slot

--- learn-vue-3-step-by-step/15/js/components/App.js
+++ learn-vue-3-step-by-step/15/js/components/App.js
@@ -1,9 +1,20 @@
 import Assignments from "./Assignments.js";
+import Panel from "./Panel.js";
 
 export default {
-    components: { Assignments },
+    components: { Assignments, Panel },
     
     template: `
-        <assignments></assignments>
+        <div class="grid gap-6">
+            <assignments></assignments>
+
+            <panel>
+                <template v-slot:heading>
+                    Hi There
+                </template>
+
+                This is my default content.
+            </panel>
+        </div>
     `,
 }
