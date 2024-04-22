bf284c8b78dc768fadf3f7cc00c2cd74df5fcaec
Robert Marquess <qowsrule@gmail.com>
2024-04-22 19:06:53 -0400

    Learn Vue 3: Step By Step - Named Slots - 5
    Use our panel component with our assignment list

--- learn-vue-3-step-by-step/15/js/components/App.js
+++ learn-vue-3-step-by-step/15/js/components/App.js
@@ -7,42 +7,6 @@ export default {
     template: `
         <div class="grid gap-6">
             <assignments></assignments>
-
-            <panel>
-                This is my default content.
-            </panel>
-
-            <panel>
-                <template v-slot:heading>
-                    Hi There
-                </template>
-
-                This is my default content.
-            </panel>
-
-            <panel>
-                <template v-slot:heading>
-                    Hi There
-                </template>
-
-                This is my default content.
-
-                <template v-slot:footer>
-                    Click here to learn more.
-                </template>
-            </panel>
-
-            <panel theme="light">
-                <template v-slot:heading>
-                    Hi There
-                </template>
-
-                This is my default content.
-
-                <template v-slot:footer>
-                    Click here to learn more.
-                </template>
-            </panel>
         </div>
     `,
 }
