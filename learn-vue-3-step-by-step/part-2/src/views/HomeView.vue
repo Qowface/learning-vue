992ecd0b073a9bb8e65883e1e2436be2c22c9359
Robert Marquess <qowsrule@gmail.com>
2024-05-17 20:24:46 -0400

    Learn Vue 3: Step By Step - Composable Example: Local Storage - 1
    Watch the values of 2 inputs and read from / write to local storage

--- learn-vue-3-step-by-step/part-2/src/views/HomeView.vue
+++ learn-vue-3-step-by-step/part-2/src/views/HomeView.vue
@@ -1,13 +1,30 @@
 <script setup>
-import { useFlash } from '@/composables/useFlash'
+import { ref, watch } from 'vue'
 
-let { flash } = useFlash()
+let food = ref(localStorage.getItem('food'))
+let age = ref(localStorage.getItem('age'))
+
+watch(food, (val) => {
+  write('food', val)
+})
+
+watch(age, (val) => {
+  write('age', val)
+})
+
+function write(key, val) {
+  localStorage.setItem(key, val)
+}
+
+setTimeout(() => {
+  food.value = 'changed'
+}, 2000)
 </script>
 
 <template>
   <main>
-    <p>
-      <button @click="flash('Test', 'It Works!')">Click Me</button>
-    </p>
+    <p>What is your favorite food? <input type="text" v-model="food" /></p>
+
+    <p>How old are you? <input type="text" v-model="age" /></p>
   </main>
 </template>
