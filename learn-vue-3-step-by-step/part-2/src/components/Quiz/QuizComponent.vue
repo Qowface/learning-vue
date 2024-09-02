e97418d3355c6c413e320671e9bad026b387992b
Robert Marquess <qowsrule@gmail.com>
2024-09-02 19:43:16 -0400

    Learn Vue 3: Step By Step - Store State in an External File
    Extract quiz data/state to its own file

--- learn-vue-3-step-by-step/part-2/src/components/Quiz/QuizComponent.vue
+++ learn-vue-3-step-by-step/part-2/src/components/Quiz/QuizComponent.vue
@@ -10,9 +10,4 @@
 import QuizHeader from '@/components/Quiz/QuizHeader.vue'
 import QuizQuestion from '@/components/Quiz/QuizQuestion.vue'
 import QuizFooter from '@/components/Quiz/QuizFooter.vue'
-import { provide } from 'vue'
-
-let props = defineProps({ quiz: Object })
-
-provide('quiz', props.quiz)
 </script>
--- learn-vue-3-step-by-step/part-2/src/views/HomeView.vue
+++ learn-vue-3-step-by-step/part-2/src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup>
 import QuizComponent from '@/components/Quiz/QuizComponent.vue'
+import { state } from '@/stores/quizStore'
 </script>
 
 <template>
   <main>
-    <QuizComponent :quiz="{ name: 'My First Quiz', questions: [] }" />
+    <QuizComponent :quiz="state" />
   </main>
 </template>
