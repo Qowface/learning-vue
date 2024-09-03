05979aebe8e08200b43819d1076dfa5f2c813c10
Robert Marquess <qowsrule@gmail.com>
2024-09-02 20:10:09 -0400

    Learn Vue 3: Step By Step - Direct Mutation Concerns
    Add counter store with both state and action

--- learn-vue-3-step-by-step/part-2/src/views/AboutView.vue
+++ learn-vue-3-step-by-step/part-2/src/views/AboutView.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { useFlash } from '@/composables/useFlash'
+import { counter } from '@/stores/counterStore'
 
 let { flash } = useFlash()
 </script>
 
 <template>
+  <p>About Page. The current count is {{ counter.count }}</p>
   <div>
     <button @click="flash('Yay!', 'It Works On The About Page!', 'info')">Click Me</button>
   </div>
