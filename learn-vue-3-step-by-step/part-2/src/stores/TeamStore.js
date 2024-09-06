9e938b4f195be73b81c1fe0d4b365b282edcd603
Robert Marquess <qowsrule@gmail.com>
2024-09-05 23:26:32 -0400

    Learn Vue 3: Step By Step - Team Dashboard Exercise - 5
    Use dedicated Pinia store for team state
    Seed store with data from JSON file

---
+++ learn-vue-3-step-by-step/part-2/src/stores/TeamStore.js
@@ -0,0 +1,19 @@
+import { defineStore } from 'pinia'
+
+export let useTeamStore = defineStore('team', {
+  state() {
+    return {
+      name: '',
+      spots: 0,
+      members: []
+    }
+  },
+
+  actions: {
+    async fill() {
+      let r = await import('@/team.json')
+
+      this.$state = r.default
+    }
+  }
+})
